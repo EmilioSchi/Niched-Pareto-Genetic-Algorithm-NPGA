f4f7aed98a41d70010e1dc2b5f5ec9a4984d2b3c
EmilioSchi <emilioschi@gmail.com>
2019-12-11 19:59:16 +0100

    setup
    i add setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setup(
+	author = 'Emilio Schinina',
+	maintainer = 'Emilio Schinina',
+	author_email = 'emilioschi@gmail.com',
+	description = 'Multi-Objective Genetic Algorithm',
+	long_description = '''
+	An implementation of Niched Pareto Genetic Algorith, a Multi-Objective
+	Genetic Algorithm invented by N. Nafploitis, J. Horn and D. E. Goldberg.
+	''',
+	license = 'Apache',
+	url = 'https://github.com/EmilioSchi/Niched-Pareto-Genetic-Algorithm-NPGA',
+	name = 'NPGA',
+	version = '0.2.1',
+	install_requires=['numpy'],
+	classifiers=("Programming Language :: Python :: 3"),
+)
